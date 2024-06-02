7e0ce312dafc652c0eecc26e971b6b0576283d44
KennY4512 <119736068+KennY4512@users.noreply.github.com>
2024-06-02 14:04:32 +0200

    Update MQTT_Module_Commande_DEV_V3.ino

--- MQTT_Module_Commande_DEV.ino
+++ MQTT_Module_Commande_DEV.ino
@@ -8,15 +8,15 @@ int statusWIFI = WL_IDLE_STATUS; // Initialisation de la variable d'état WiFi e
 
 // WiFi
 WiFiClient clientwifi; // Création d'un client WiFi (utilisant la bibliothèque <ESP8266WiFi.h>)
-#define SSID_WIFI "" // SSID du réseau WiFi auquel se connecter
-#define MDP_WIFI "" // Mot de passe du réseau WiFi auquel se connecter
+#define SSID_WIFI "WiFi-QDA" // SSID du réseau WiFi auquel se connecter
+#define MDP_WIFI "Qda42000!" // Mot de passe du réseau WiFi auquel se connecter
 
 // MQTT
 PubSubClient client(clientwifi); // Création d'un client MQTT (utilisant la bibliothèque <ESP8266WiFi.h>) utilisant le client WiFi créée précédemment 
 char ipsrvMQTT[]  = "qda.duckdns.org"; // Adresse IP du serveur MQTT
 int portsrvMQTT  = 1883; // Port du serveur MQTT
 char idModuleCommande[]  = "moduleCommande01"; // Identifiant du module de commande
-char topic[]  = "/qda/moduleCommande01"; // Topic MQTT auquel s'abonner
+char topic[]  = "qda/moduleCommande01"; // Topic MQTT auquel s'abonner
 char utilisateursrvMQTT[]  = "root"; // Utilisateur du serveur MQTT
 char mdpsrvMQTT[]  = "Passw0rd%1"; // Mot de passe du serveur MQTT
 const String msgdesactivationVMC = "OFF"; // Message pour désactiver la VMC
